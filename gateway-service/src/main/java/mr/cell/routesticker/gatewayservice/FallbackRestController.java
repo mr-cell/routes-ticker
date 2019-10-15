76fdbc99f8cf3b9622e58da614d4b60048470653
mr-cell <marcel.schally@gmail.com>
2019-10-16 00:32:06 +0200

    first pass on service-discovery-aware gateway service

--- gateway-service/src/main/java/mr/cell/routesticker/gatewayservice/FallbackRestController.java
+++ gateway-service/src/main/java/mr/cell/routesticker/gatewayservice/FallbackRestController.java
@@ -2,12 +2,13 @@ package mr.cell.routesticker.gatewayservice;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class FallbackRestController {
 
     @RequestMapping("/fallback")
-    public String fallback() {
-        return "Fallback";
+    public Mono<String> fallback() {
+        return Mono.just("Fallback");
     }
 }
--- gateway-service/src/main/java/mr/cell/routesticker/gatewayservice/GatewayConfiguration.java
+++ gateway-service/src/main/java/mr/cell/routesticker/gatewayservice/GatewayConfiguration.java
@@ -1,5 +1,7 @@
 package mr.cell.routesticker.gatewayservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,9 +10,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayConfiguration {
 
+    @Autowired
+    DiscoveryClient discoveryClient;
+
     @Bean
     public RouteLocator gatewayRoutes(final RouteLocatorBuilder builder) {
         return builder.routes()
+                .route(p -> p.path("/users").uri("lb://usersservice/users"))
                 .route(p -> p
                         .path("/get")
                         .filters(f -> f.addRequestHeader("Hello", "World"))
