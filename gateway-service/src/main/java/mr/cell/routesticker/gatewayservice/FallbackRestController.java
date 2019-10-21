48c4d5b5bb4f39cd1c85bcdaef91e6e6d43a46fe
mr-cell <marcel.schally@gmail.com>
2019-10-21 16:38:31 +0200

    added proper config for gateway service and disabled accessing other functional services directly

--- gateway-service/src/main/java/mr/cell/routesticker/gatewayservice/FallbackRestController.java
+++ gateway-service/src/main/java/mr/cell/routesticker/gatewayservice/FallbackRestController.java
@@ -1,14 +1,25 @@
 package mr.cell.routesticker.gatewayservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
 
 @RestController
+@RequestMapping("/fallback")
 public class FallbackRestController {
+    private static final Logger LOG = LoggerFactory.getLogger(FallbackRestController.class);
 
-    @RequestMapping("/fallback")
-    public Mono<String> fallback() {
-        return Mono.just("Fallback");
+    @RequestMapping("/users")
+    public Flux<Void> usersFallback() {
+        LOG.warn("Invoking fallback for /users endpoint");
+        return Flux.empty();
+    }
+
+    @RequestMapping("/routes")
+    public Flux<Void> routesFallback() {
+        LOG.warn("Invoking fallback for /routes endpoint");
+        return Flux.empty();
     }
 }
--- gateway-service/src/main/java/mr/cell/routesticker/gatewayservice/GatewayConfiguration.java
+++ gateway-service/src/main/java/mr/cell/routesticker/gatewayservice/GatewayConfiguration.java
@@ -1,7 +1,8 @@
 package mr.cell.routesticker.gatewayservice;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandKey;
+import com.netflix.hystrix.HystrixObservableCommand;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -10,23 +11,37 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayConfiguration {
 
-    @Autowired
-    DiscoveryClient discoveryClient;
-
     @Bean
     public RouteLocator gatewayRoutes(final RouteLocatorBuilder builder) {
         return builder.routes()
-                .route(p -> p.path("/users").uri("lb://usersservice/users"))
                 .route(p -> p
-                        .path("/get")
-                        .filters(f -> f.addRequestHeader("Hello", "World"))
-                        .uri("http://httpbin.org:80"))
+                        .path("/users")
+                        .filters(f -> f
+                                .hystrix(config -> config
+                                        .setName("usersFallback")
+                                        .setFallbackUri("forward:/fallback/users")
+                                        .setSetter(HystrixObservableCommand.Setter
+                                                .withGroupKey(createHystrixCommandGroupKey("routes-ticker"))
+                                                .andCommandKey(createHystrixCommandKey("users")))))
+                        .uri("lb://usersservice/users"))
                 .route(p -> p
-                        .host("*.hystrix.com")
-                        .filters(f -> f.hystrix(config -> config
-                                .setName("mycmd")
-                                .setFallbackUri("forward:/fallback")))
-                        .uri("http://httpbin.org:80"))
+                        .path("/routes")
+                        .filters(f -> f
+                                .hystrix(config -> config
+                                        .setName("routesFallback")
+                                        .setFallbackUri("forward:/fallback/routes")
+                                        .setSetter(HystrixObservableCommand.Setter
+                                                .withGroupKey(createHystrixCommandGroupKey("routes-ticker"))
+                                                .andCommandKey(createHystrixCommandKey("routes")))))
+                        .uri("lb://climbingroutesservice/routes"))
                 .build();
+    }
+
+    private HystrixCommandGroupKey createHystrixCommandGroupKey(final String key) {
+        return HystrixCommandGroupKey.Factory.asKey(key);
+    }
+
+    private HystrixCommandKey createHystrixCommandKey(final String key) {
+        return HystrixCommandKey.Factory.asKey(key);
     }
 }
